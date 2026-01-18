b094e253cc7f8709a700521a06434c59c8213992
Blessed Friday <fridayblessed333@gmail.com>
2026-01-18 18:24:26 +0100

    updated

--- Students/obj/Debug/net10.0/Students.AssemblyInfo.cs
+++ Students/obj/Debug/net10.0/Students.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Students")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ec47ee862c53cfea7ed6480aef639cb04382691")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a018a7341f2cf1f5041c9b1e9a7b80910808e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Students")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Students")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
